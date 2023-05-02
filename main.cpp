e008221ac9de5bd11ef82a044ceed5f3544d5dfb
ZuxiGIT <shim.01@mail.ru>
2023-05-03 02:34:50 +0300

    deleted useless file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "crypto.hpp"
 // #include "net.hpp"
 #include <stdio.h>
+#include <vector>
 #include <tchar.h>
 
 #define UNUSED(x) (void)(x)
@@ -9,64 +10,68 @@
 int main(void) 
 { 
 
-    HCRYPTPROV hCryptoProv  = crypto::getCryptoProv(TEXT("Key Containter for Encoding/Decoding"), crypto::ProvType::RSA);
-    HCRYPTKEY hExchKey = crypto::getKeyPair(crypto::KeyPairType::Exchange,hCryptoProv);
-    HCRYPTKEY hSigKey = crypto::getKeyPair(crypto::KeyPairType::Signature, hCryptoProv);
-    HCRYPTKEY hSesKey = crypto::getSessionKey(hCryptoProv);
+//     HCRYPTPROV hCryptoProv  = crypto::getCryptoProv(TEXT("Key Containter for Encoding/Decoding"), crypto::ProvType::RSA);
+//     HCRYPTKEY hExchKey = crypto::getKeyPair(crypto::KeyPairType::Exchange,hCryptoProv);
+//     HCRYPTKEY hSigKey = crypto::getKeyPair(crypto::KeyPairType::Signature, hCryptoProv);
+//     HCRYPTKEY hSesKey = crypto::getSessionKey(hCryptoProv);
 
-    BYTE* pbBlob = NULL;
+//     BYTE* pbBlob = NULL;
 
-    DWORD dwBlobSize = crypto::exportKey(hSesKey, hExchKey, crypto::ExportKeyType::Session, &pbBlob);
+//     DWORD dwBlobSize = crypto::exportKey(hSesKey, hExchKey, crypto::ExportKeyType::Session, &pbBlob);
 
-    printf("Blob size is %li\n", dwBlobSize);
-    printf("Blob:\n");
-    for(DWORD i = 0; i < dwBlobSize; ++i)
-        printf("%hhx ", pbBlob[i]);
-    printf("\n");
+//     printf("Blob size is %li\n", dwBlobSize);
+//     printf("Blob:\n");
+//     for(DWORD i = 0; i < dwBlobSize; ++i)
+//         printf("%hhx ", pbBlob[i]);
+//     printf("\n");
 
-    HCRYPTKEY hImpSesKey = crypto::importKey(hCryptoProv, hExchKey, pbBlob, dwBlobSize);
-    BYTE* pbBlob2 = NULL;
+//     HCRYPTKEY hImpSesKey = crypto::importKey(hCryptoProv, hExchKey, pbBlob, dwBlobSize);
+//     BYTE* pbBlob2 = NULL;
 
-    DWORD dwBlobSize2 = crypto::exportKey(hImpSesKey, hExchKey, crypto::ExportKeyType::Session, &pbBlob2);
-    printf("Blob size is %li\n", dwBlobSize2);
-    printf("Blob:\n");
-    for(DWORD i = 0; i < dwBlobSize2; ++i)
-        printf("%hhx ", pbBlob2[i]);
-    printf("\n");
+//     DWORD dwBlobSize2 = crypto::exportKey(hImpSesKey, hExchKey, crypto::ExportKeyType::Session, &pbBlob2);
+//     printf("Blob size is %li\n", dwBlobSize2);
+//     printf("Blob:\n");
+//     for(DWORD i = 0; i < dwBlobSize2; ++i)
+//         printf("%hhx ", pbBlob2[i]);
+//     printf("\n");
 
 
 
-    unsigned char test[] = "hello";
-    printf("before encryption:\n");
-    for(DWORD i = 0; i < sizeof(test); ++i)
-        printf("0x%hhX ", test[i]);
-    printf("\n");
+//     unsigned char test[] = "hello";
+//     printf("before encryption:\n");
+//     for(DWORD i = 0; i < sizeof(test); ++i)
+//         printf("0x%hhX ", test[i]);
+//     printf("\n");
 
-    crypto::encryptData(hSesKey, test, sizeof(test));
+//     crypto::encryptData(hSesKey, test, sizeof(test));
 
-    printf("\nafter encryption and before decryption:\n");
-    for(DWORD i = 0; i < sizeof(test); ++i)
-        printf("0x%hhX ", test[i]);
-    printf("\n");
+//     printf("\nafter encryption and before decryption:\n");
+//     for(DWORD i = 0; i < sizeof(test); ++i)
+//         printf("0x%hhX ", test[i]);
+//     printf("\n");
 
-    crypto::decryptData(hSesKey, test, sizeof(test));
+//     crypto::decryptData(hSesKey, test, sizeof(test));
 
-    printf("\nafter decryption:\n");
-    for(DWORD i = 0; i < sizeof(test); ++i)
-        printf("0x%hhX ", test[i]);
-    printf("\n");
+//     printf("\nafter decryption:\n");
+//     for(DWORD i = 0; i < sizeof(test); ++i)
+//         printf("0x%hhX ", test[i]);
+//     printf("\n");
 
-    delete [] pbBlob;
-    delete [] pbBlob2;
+//     delete [] pbBlob;
+//     delete [] pbBlob2;
 
-#define STR(x) #x
-    printf(__FUNCTION__);
+// #define STR(x) #x
+//     printf(__FUNCTION__);
 
-    crypto::destroyKey(hExchKey);
-    crypto::destroyKey(hSigKey);
-    crypto::destroyKey(hSesKey);
-    crypto::destroyKey(hImpSesKey);
-    crypto::releaseCryptoProv(hCryptoProv);
+//     crypto::destroyKey(hExchKey);
+//     crypto::destroyKey(hSigKey);
+//     crypto::destroyKey(hSesKey);
+//     crypto::destroyKey(hImpSesKey);
+//     crypto::releaseCryptoProv(hCryptoProv);
 
-    _tprintf(TEXT("Everything is okay."));  
+//     _tprintf(TEXT("Everything is okay."));  
+    printf("sizeof: %zu\n", sizeof(WORD));
+    printf("sizeof: %zu\n", sizeof(DWORD));
+    printf("sizeof: %zu\n", sizeof(std::vector<int>*));
+    printf("sizeof: %zu\n", sizeof(std::vector<int>));
 } 
